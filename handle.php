b79888ddfa154fd246a86c751ebb8d16e2cbbcc3
xuwenzhi <358350782@qq.com>
2016-01-10 13:27:05 +0000

    up

---
+++ handle.php
@@ -0,0 +1,20 @@
+<?php
+
+$string = file_get_contents("latlng.php");
+
+//var_dump($string);
+
+$arr = explode("\n", $string);
+
+
+var_dump($arr);
+
+$ret = "";
+foreach($arr as $v){
+	if($v != ''){
+		$latlng = explode(",", $v);
+		$ret .= ','.$latlng[0].','. $latlng[1]. ',0.1';
+	}
+}
+
+var_dump($ret);
